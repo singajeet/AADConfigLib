83f6a0d6311f87e69393cbeec1b735d487195379
Admin <Admin@Ajeet>
2017-08-20 22:39:26 +0530

    Initial commit

---
+++ AADConfigLib.h
@@ -0,0 +1,19 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _AADConfigLib_H_
+#define _AADConfigLib_H_
+#include "Arduino.h"
+//add your includes for the project AADConfigLib here
+
+#define CONF_LCD_DRIVER new Nokia1616Driver();
+#define CONF_DEFAULT_FONT new Nokia1616EmbeddedFont8x15();
+
+
+
+
+//Do not add code below this line
+#endif /* _AADConfigLib_H_ */
